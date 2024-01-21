c1ed2c4c88bc38e2110c91f7881842c4b62d9dc5
SirMarouane <m.elbattioui@hotmail.com>
2024-01-21 23:49:10 +0100

    end

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
+let color = ["red", "green", "blue", "yellow", "orange", "purple", "cyan", "pink", "brown", "teal", "maroon", "olive", "navy", "violet", "indigo"];
+
 let section = document.querySelector("section")
 let div
 
-for (let i = 0; i < 2024; i++) {
+for (let i = 0; i < 696; i++) {
 
     div = document.createElement("div")
 
@@ -11,7 +13,22 @@ for (let i = 0; i < 2024; i++) {
 
 let newDiv = document.querySelectorAll("div")
 
-newDiv.addEventListener("mouseover", () => {
-    newDiv.style.background = "red"
 
-})
+newDiv.forEach(element => {
+    element.addEventListener("mouseover", () => {
+        element.style.background = color[Math.floor(Math.random()*color.length)]
+       
+        
+    })
+
+    element.addEventListener("mouseout", () => {
+        element.style.background = ""
+        element.style.transition = "1.5s"
+        square.style.boxShadow = "0 0 2px, 0 0 10px"
+    
+       
+    })
+    
+});
+
+
